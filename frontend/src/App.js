9718669a01ac8d6c5b92c9461d1fc1cc875f6a72
Jeferson <jefersonborba@protonmail.com>
2020-12-24 23:21:55 -0300

    feature: created the header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Container } from "react-bootstrap";
+
 const App = () => {
   return (
     <div className="App">
-      
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>
+            Welcome to the Shop
+          </h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
